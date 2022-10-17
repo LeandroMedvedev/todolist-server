8e8e9f4323a691b567cb5d95589cd9e2d1b484a6
Leandro Medvedev <leandromedvedev@hotmail.com>
2022-10-17 04:54:47 -0300

    deploy

--- src/migrations/1665993224002-initialCommit.ts
+++ src/migrations/1665993224002-initialCommit.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class initialCommit1665992382752 implements MigrationInterface {
+export class initialCommit1665993224002 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
     }
