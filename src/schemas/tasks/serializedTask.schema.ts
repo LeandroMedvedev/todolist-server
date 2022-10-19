7478548d3c7c61cb753723d0fe3115c304e6181b
Leandro Medvedev <leandromedvedev@hotmail.com>
2022-10-19 13:25:31 -0300

    feat: expires in (token) changed

--- src/schemas/tasks/serializedTask.schema.ts
+++ src/schemas/tasks/serializedTask.schema.ts
@@ -1,10 +1,10 @@
 import { boolean, date, object, string } from 'yup';
 
 const serializedTaskSchema = object().shape({
-  // taskUuid: string().uuid().required(),
+  taskUuid: string().uuid().required(),
   description: string().required(),
-  // createdAt: date().required(),
-  // completed: boolean().required(),
+  createdAt: date().required(),
+  completed: boolean().required(),
 });
 
 export default serializedTaskSchema;
