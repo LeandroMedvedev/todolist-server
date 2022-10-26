281ed271dde031691cae46f8bd1673cda7b2fdc1
Leandro Medvedev <leandromedvedev@hotmail.com>
2022-10-26 17:25:36 -0300

    docs: swagger

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import 'express-async-errors';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import { errorHandler } from './errors';
+import swaggerUiExpress from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 import appRoutes from './routes';
 
@@ -9,6 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 appRoutes(app);
+
+app.use(
+  '/api-documentation',
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup(swaggerDocument)
+);
 
 app.use((error: Error, _: Request, response: Response, __: NextFunction) => {
   return errorHandler(error, response);
