f4bff2876dab89bede7f2850966560791c60031a
Leandro Medvedev <leandromedvedev@hotmail.com>
2022-11-14 17:49:14 -0300

    refactor: app.options(*, cors()) in app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,15 @@ import swaggerDocument from './swagger.json';
 import appRoutes from './routes';
 
 const app = express();
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(express.json());
-app.use(cors());
-app.options('*', cors());
 appRoutes(app);
 
 app.use(
